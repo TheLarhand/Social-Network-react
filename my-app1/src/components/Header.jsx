2b9bbc9673a17de8d4e88f9153334de0ad61e6bd
TheLarhand <sokoligor2000@gmail.com>
2022-07-24 23:19:01 +0600

    0.1 v

---
+++ my-app1/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Open_Broadcaster_Software_Logo.png/2048px-Open_Broadcaster_Software_Logo.png' />
+      </header>
+    )
+}
+
+export default Header;
