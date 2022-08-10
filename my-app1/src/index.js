de8a5325b0710a126b5ca161052cc987bf3189ee
Игорь Соколов <@itstep.krg>
2022-08-10 15:56:24 +0600

    0.14 v delete render.js

--- my-app1/src/index.js
+++ my-app1/src/index.js
@@ -1,12 +1,33 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
-import { rerenderEntireTree } from './render';
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { addMessage, addPost, updateNewMessageText, updateNewPostText } from './redux/state';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-rerenderEntireTree(state);
+export let rerenderEntireTree = (state) => {
+  
+  root.render(
+    <React.StrictMode>
+      <App 
+      state={state} 
+      addPost={addPost} 
+      updateNewPostText={updateNewPostText} 
+      addMessage = {addMessage}
+      updateNewMessageText = {updateNewMessageText}
+      />
+    </React.StrictMode>
+  );
+}
+rerenderEntireTree(state)
+
+subscribe()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(rerenderEntireTree);
